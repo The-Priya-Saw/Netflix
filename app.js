4b60348ece5537e193b77f09af0ec8177561749c
priya saw <priyasaw2452gmail.com>
2023-01-06 14:33:14 +0530

    changed port no to 80

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const bodyParser = require("body-parser");
 const env = require("dotenv");
 const fs = require("fs");
 env.config();
-const port = process.env.PORT | 3000;
+const port = process.env.PORT | 80;
 const cookieSession = require("cookie-session");
 const jwt = require("jsonwebtoken");
 const app = express();
